78df6f782bd7d85f7162128758426d5dce828a94
kenzosashida090 <kenzosashida1@gmail.com>
2025-02-19 09:29:31 -0600

    [new] New project, also i already include a navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { NavLink } from "react-router"
+
+
+
+const NavBar: React.FC= () => {
+    return (
+        <header className="w-full px-8 bg-gray-100 text-gray-700 shadow-md">
+            <div className="container flex flex-col md:flex-row items-center justify-between py-4 max-w-7xl ">
+             <div className="flex flex-col md:flex-row items-center">
+                <NavLink to="/" className="flex flex-col md:flex-row items-center"><span className="text-xl font-black ">REACT<span className="text-blue-400">Explorer</span></span></NavLink>
+                <nav className="flex flex-wrap items-center ml-0 md:ml-8 md:border-l md:pl-8">
+                    <NavLink
+                    to="/"
+                    end
+                    className="mr-5 font-medium text-gray-600 hover:text-gray-900 ease-out duration-300 transition-all"
+                    >
+                    Home
+                    </NavLink>
+                    <NavLink
+                    to="/countries"
+                    className="mr-5 font-medium text-gray-600 hover:text-gray-900 ease-out duration-300 transition-all"
+                    >
+                    Countries
+                    </NavLink>
+                    <NavLink
+                    to="/about"
+                    className="mr-5 font-medium text-gray-600 hover:text-gray-900 ease-out duration-300 transition-all"
+                    >
+                    About
+                    </NavLink>
+                </nav>
+            </div>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar
