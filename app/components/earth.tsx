3895a33e5f701f6c53540362dca1abf2f27de658
kenzosashida090 <kenzosashida1@gmail.com>
2025-02-25 11:52:48 -0600

    [new] Adding rotation animation

--- app/components/earth.tsx
+++ app/components/earth.tsx
@@ -8,8 +8,14 @@ const Earth: React.FC = ()=> {
     // const colorMap = useLoader(TextureLoader, "./earth_albedo.jpg")
     // const bumpMap = useLoader(TextureLoader, "./earth_bump.jpg")
     // const specularMap = useLoader(TextureLoader, "./earth_specular.png")
-    // const earthRef = React.useRef<any>(null);
-
+    const earthRef = React.useRef<any>(null);
+    useFrame(() => {
+            if (earthRef.current) {
+              // Gira la Tierra alrededor del eje Y (vertical)
+              earthRef.current.rotation.y += 0.002;
+              
+            }
+        })
 
     // useFrame(() => {
     //     if (earthRef.current) {
@@ -31,7 +37,7 @@ const Earth: React.FC = ()=> {
     // )
     const {scene}  = useGLTF('./scene.gltf')
     return(
-        <primitive object={scene} scale={1} position={[0.5,0.2,0.6]}/>
+        <primitive ref={earthRef} object={scene} scale={1} position={[0.4,-1.8,0.1]}/>
     )
 }
 
@@ -44,10 +50,9 @@ const Scene : React.FC = () => {
         //     <directionalLight position={[5,5,3]} intensity={9}/>
         //     <Earth />
         // </Canvas>
-        <Canvas    style={{ width: '100%', height: '300px' }} camera={{ position: [2, 5, 2], fov: 60 }}>
+        <Canvas  className='flex items-center justify-center'  style={{ width: '100%', height: '300px' }} camera={{ position: [2, 5, 2], fov: 40 }}>
             <ambientLight intensity={0.5} />
             <directionalLight position={[5, 5, 5]} intensity={1} />
-           
             <Earth />
         </Canvas>
     )
